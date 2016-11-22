6fdbb13f24836f2057f4aa70bc131871949b36ef
fabianbrash <fabianbrash@gmail.com>
2016-11-22 10:26:32 -0500

    Added GroupPolicy.ps1 and updated FolderSize.ps1

---
+++ FolderSize.ps1
@@ -0,0 +1,35 @@
+﻿#FileName:      FolderSize.ps1
+#Author:        Fabian Brash
+#Date:          11-16-2016
+#Modified:      11-16-2016
+
+<# "{0:C0}" - Currency
+   "{0:D2}" - Decimal
+   "{0:P0}" - Percent
+   "{0:X0}" - Hexadecimal
+    
+
+
+<#________   ________   ________      
+|\   __  \ |\   __  \ |\   ____\     
+\ \  \|\  \\ \  \|\  \\ \  \___|_    
+ \ \   _  _\\ \   ____\\ \_____  \   
+  \ \  \\  \|\ \  \___| \|____|\  \  
+   \ \__\\ _\ \ \__\      ____\_\  \ 
+    \|__|\|__| \|__|     |\_________\
+                         \|_________|#>
+                                     
+                                     
+
+Clear-Host
+
+$Folder = 'C:\Source'
+$num = 19245664285559
+
+$colItems = (Get-ChildItem $Folder -Recurse | Measure-Object -Property Length -Sum)
+"{0:N3}" -f ($colItems.sum / 1MB) + " MB"
+
+Write-Host "`n"
+"{0:C2}" -f $num
+$num
+Write-Host "`n"
---
+++ GroupPolicy.ps1
@@ -0,0 +1,39 @@
+﻿
+
+Clear-Host
+
+
+<#
+    Valid values are AllSettingsEnabled, AllSettingsDisabled, UserSettingsDisabled and ComputerSettingsDisabled.
+#>
+
+
+
+
+try
+    {
+        Import-Module -Name ActiveDirectory -ErrorAction Stop
+        Import-Module -Name GroupPolicy -ErrorAction Stop
+     }
+catch
+    {
+        Write-Error -Message "Could not find and load modules"
+    }
+
+
+$gpoStatus = Get-GPO "RPS-Student-WorkstationSecurity"
+
+$gpoStatus.GpoStatus
+
+if($gpoStatus.GpoStatus -eq 'AllSettingsDisabled')
+    {
+        Write-Host "Success it's off"
+    }
+else
+    {
+        Write-Host "Still on!!"
+    }
+
+<# We can set the status like
+    $gpoStatus.GpoStatus = "AllSettingsEnabled"
+#>
